a16826b5fb52a1b214a1d80674faba22fdf728f9
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-27 00:20:16 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,7 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+
+const myNumber = 5.6591
+// const output = Math.floor(myNumber);
+const output = Math.ceil(myNumber);
+console.log(output);
